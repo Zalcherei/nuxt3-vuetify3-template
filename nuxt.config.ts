4036c7ebf391c6c60f985d8d2dd5f0dacb605c23
Zalcherei <45491430+Zalcherei@users.noreply.github.com>
2022-11-02 16:10:39 +0200

    4 files modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,8 @@
-import vuetify from 'vite-plugin-vuetify';
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 // @ts-ignore
 export default defineNuxtConfig({
   css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.min.css'],
-  vite: {
-    // @ts-ignore
-    ssr: {
-      noExternal: ['vuetify'],
-    },
+  build: {
+    transpile: ['vuetify'],
   },
-  modules: [
-    // @ts-ignore
-    async (options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) =>
-        config.plugins.push(vuetify())
-      );
-    },
-  ],
 });
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,11 +1,14 @@
 import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     theme: {
       defaultTheme: 'dark',
     },
+    components,
+    directives,
   });
-
   nuxtApp.vueApp.use(vuetify);
 });
